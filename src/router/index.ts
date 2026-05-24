f6ebf596d6754eb18493fa5dae4c7772397509e9
ekureaa <account@ekurea.net>
2026-05-25 03:24:39 +0900

    各ページのページタイトルを変更

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,17 +4,23 @@ import About from '@/pages/About.vue'
 import Photo from '@/pages/Photo.vue'
 import Links from '@/pages/Links.vue'
 
+const siteTitle = 'ekurea.net'
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', name: 'home', component: Home },
-    { path: '/about', name: 'about', component: About },
-    { path: '/photo', name: 'photo', component: Photo },
-    { path: '/links', name: 'links', component: Links },
+    { path: '/', name: 'home', component: Home, meta: { title: siteTitle } },
+    { path: '/about', name: 'about', component: About, meta: { title: `About | ${siteTitle}` } },
+    { path: '/photo', name: 'photo', component: Photo, meta: { title: `Photo | ${siteTitle}` } },
+    { path: '/links', name: 'links', component: Links, meta: { title: `Links | ${siteTitle}` } },
   ],
   scrollBehavior() {
     return { top: 0 }
   },
+})
+
+router.afterEach((to) => {
+  document.title = typeof to.meta.title === 'string' ? to.meta.title : siteTitle
 })
 
 export default router
